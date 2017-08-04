6697d258bcf58a19e7a36a4125f9bc91951faaa6
MislavPis <30667666+MislavPis@users.noreply.github.com>
2017-08-04 16:04:52 +0200

    Add files via upload

---
+++ eposta/newMessage.php
@@ -0,0 +1,18 @@
+<?php
+$action=$_REQUEST['action'];
+    
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Poruka sa web stranice!";
+        mail("ojbudizdrav@gmail.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+ 
+?> 
